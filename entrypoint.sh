9eda3543c3f7dbc6514802ae5c5e0d37cb6a49ba
somaz <genius5711@gmail.com>
2024-06-21 17:55:36 +0900

    fix: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 echo "Debug: Starting script execution."
 
-# Read conditions and corresponding results into arrays
 IFS=',' read -ra conditions <<<"$INPUT_CONDITIONS"
 IFS=',' read -ra true_values <<<"$INPUT_TRUE_VALUES"
 IFS=',' read -ra false_values <<<"$INPUT_FALSE_VALUES"
@@ -11,14 +10,12 @@ IFS=',' read -ra false_values <<<"$INPUT_FALSE_VALUES"
 function replace_placeholders {
 	local condition="$1"
 	for varname in $(echo "$condition" | grep -oE '\b[A-Z_]+\b'); do
-		local value="${!varname}"
-
-		# Handle escaping of characters that could interfere with sed
-		local escaped_value="${value//\//\\/}"  # Escape slashes for safe usage in sed
-		escaped_value="${escaped_value//&/\\&}" # Escape ampersands due to their special meaning in sed
-
-		# Replace the placeholder with the actual value using sed for better pattern matching
-		condition=$(echo "$condition" | sed "s/\b$varname\b/$escaped_value/g")
+		local value="${!varname:-unknown}" # Use a default of 'unknown' if the variable is not set
+		# Escaping special characters in the value that might affect direct replacement
+		local escaped_value="${value//\//\\/}"  # Escape slashes for safe usage in replacements
+		escaped_value="${escaped_value//&/\\&}" # Escape ampersands which have special meaning in replacements
+		# Replace the variable name with its value in the condition, using bash pattern substitution
+		condition="${condition//"$varname"/"$escaped_value"}"
 	done
 	echo "$condition"
 }
@@ -29,9 +26,8 @@ for i in "${!conditions[@]}"; do
 
 	# Replace placeholders and form the correct conditional expression
 	dynamic_condition=$(replace_placeholders "${conditions[i]}")
-	echo "Evaluated condition: $dynamic_condition"
 
-	# Evaluate the condition
+	# Evaluate the condition using eval to interpret the condition as an executable statement
 	if eval "[[ $dynamic_condition ]]"; then
 		result="${true_values[i]}"
 		echo "Debug: Condition $i evaluated to true."
